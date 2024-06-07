c5a46bb9a0ab7b9a6a543db1c4e53c3cdca6a223
Vedant Sharma <vedantsharma@Vedants-Air.bbrouter>
2024-06-08 02:09:26 +0530

    made global state... used sys/ioctl to find win dimensions

--- zen.c
+++ zen.c
@@ -5,33 +5,40 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 // defines
 #define CTRL_KEY(k) ((k) & 0x1f)
 // data
-struct termios orig_termios;
+struct editorConfig
+{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+struct editorConfig E;
 // terminal
 void die(const char *s)
 {
     write(STDOUT_FILENO, "\x1b[2J", 4); // clears screen
-    write(STDOUT_FILENO, "\1xb[H", 3);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
 
 void disableRawMode()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     {
         die("tcsetattr");
     }
 }
 void enableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
     atexit(disableRawMode);
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // for cntrl s and q and m (10->13)
     raw.c_oflag &= ~(OPOST);                                  // disables output processing
     raw.c_cflag |= (CS8);
@@ -53,6 +60,20 @@ char editorReadKey()
     }
     return c;
 }
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
 // input
 void editorProcessKeypress()
 {
@@ -61,7 +82,7 @@ void editorProcessKeypress()
     {
     case CTRL_KEY('x'):
         write(STDOUT_FILENO, "\x1b[2J", 4); // clears screen
-        write(STDOUT_FILENO, "\1xb[H", 3);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }
@@ -70,23 +91,33 @@ void editorProcessKeypress()
 void editorDrawRows()
 {
     int y;
-    for (y = 0; y < 24; y++)
+    for (y = 0; y < E.screenrows; y++)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+        if (y < E.screenrows - 1)
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 void editorRefreshScreen()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4); // clears screen
-    write(STDOUT_FILENO, "\1xb[H", 3);  // cursor back to starting
+    write(STDOUT_FILENO, "\x1b[H", 3);  // cursor back to starting
     editorDrawRows();
-    write(STDOUT_FILENO, "\1xb[H", 3);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 // init
+void initEditor()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1)
     {
