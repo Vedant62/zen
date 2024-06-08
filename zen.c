684b15efcdc900082fb9cedadd289bc14c69a8c0
Vedant Sharma <vedantsharma@Vedants-Air.bbrouter>
2024-06-09 03:06:53 +0530

    cursor now moves using the arrow keys

--- zen.c
+++ zen.c
@@ -11,9 +11,17 @@
 // defines
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ZEN_VERSION "0.0.1";
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
 // data
 struct editorConfig
 {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -23,7 +31,7 @@ struct editorConfig E;
 void die(const char *s)
 {
     write(STDOUT_FILENO, "\x1b[2J", 4); // clears screen
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    write(STDOUT_FILENO, "\x1b[H", 3);  // cursor wapas upar
     perror(s);
     exit(1);
 }
@@ -51,7 +59,7 @@ void enableRawMode()
         die("tcsetattr");
 }
 
-char editorReadKey()
+int editorReadKey()
 {
     int nread;
     char c;
@@ -60,7 +68,33 @@ char editorReadKey()
         if (nread == -1 && errno != EAGAIN)
             die("read");
     }
-    return c;
+    if (c == '\x1b')
+    {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+        if (seq[0] == '[')
+        {
+            switch (seq[1])
+            {
+            case 'A':
+                return ARROW_UP;
+            case 'B':
+                return ARROW_DOWN;
+            case 'C':
+                return ARROW_RIGHT;
+            case 'D':
+                return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
 }
 int getWindowSize(int *rows, int *cols)
 {
@@ -99,18 +133,43 @@ void abFree(struct abuf *ab)
     free(ab->b);
 }
 // input
+void editorMoveCursor(int key)
+{
+    switch (key)
+    {
+    case ARROW_UP:
+        E.cy--;
+        break;
+    case ARROW_LEFT:
+        E.cx--;
+        break;
+    case ARROW_DOWN:
+        E.cy++;
+        break;
+    case ARROW_RIGHT:
+        E.cx++;
+        break;
+    }
+}
 void editorProcessKeypress()
 {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c)
     {
     case CTRL_KEY('x'):
         write(STDOUT_FILENO, "\x1b[2J", 4); // clears screen
-        write(STDOUT_FILENO, "\x1b[H", 3);
+        write(STDOUT_FILENO, "\x1b[H", 3);  // cursor upar
         exit(0);
+        break;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
         break;
     }
 }
+
 // output
 void editorDrawRows(struct abuf *ab)
 {
@@ -155,7 +214,10 @@ void editorRefreshScreen()
     // abAppend(&ab, "\x1b[2J", 4); // clears screen
     abAppend(&ab, "\x1b[H", 3); // cursor back to starting
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1); // this is H command with arguments specifying the cood of cursor
+    abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -164,6 +226,8 @@ void editorRefreshScreen()
 // init
 void initEditor()
 {
+    E.cx = 0;
+    E.cy = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
